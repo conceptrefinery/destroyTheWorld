68a58a1f07908b21799e681105d054af5e0747a3
Chris Common <chriscommon@Chriss-iMac.local>
2017-11-07 15:48:43 +1100

    hello world

--- destroyTheWorld/ViewController.swift
+++ destroyTheWorld/ViewController.swift
@@ -18,7 +18,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        print("hello world")
     }
+    
+    
 
 
 }
